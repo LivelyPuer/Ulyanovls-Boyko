270204e641d53978fa19d2f9f09e27247f4c2fa2
LivelyPuer <ma15ks35i6boy@gmail.com>
2020-12-06 16:26:16 +0400

    My three git-repo

--- pyQt5/main.py
+++ pyQt5/main.py
@@ -1,19 +1,36 @@
 import sys
 from random import randrange
-
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtCore import QPointF
-from PyQt5.QtGui import QPainter, QPolygonF, QColor
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from PyQt5 import QtCore, QtWidgets
 
-class MyWidget(QMainWindow):
+
+class Ui_Form:
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(487, 625)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(180, 20, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Клик!"))
+        self.pushButton.setText(_translate("Form", "Клик!"))
+
+
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
         self.size = 0
+        self.color = QColor(0, 0, 0)
         # Обратите внимание: имя элемента такое же как в QTDesigner
 
     def paintEvent(self, event):
@@ -26,11 +43,13 @@ class MyWidget(QMainWindow):
     def paint(self):
         self.do_paint = True
         self.size = randrange(100, 250)
+        self.color = QColor(randrange(0, 255), randrange(0, 255),
+                            randrange(0, 255))
         self.repaint()
 
     def draw(self):
         # Задаем кисть
-        self.qp.setBrush(QColor("#FFFF00"))
+        self.qp.setBrush(self.color)
         s = (self.size // 2)
         x, y = (200, 300)
         self.qp.drawEllipse(int(x - s), int(y - s),
